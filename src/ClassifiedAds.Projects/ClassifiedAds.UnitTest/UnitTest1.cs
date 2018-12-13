45a0841b642dd2d15c2f8caf6f24e230c18e6c81
Phong Nguyen <nguyendoanphong.it@gmail.com>
2018-12-13 12:48:06 +0700

    Updated Dispatcher and Added Unit Test project

---
+++ src/ClassifiedAds.Projects/ClassifiedAds.UnitTest/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ClassifiedAds.UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
