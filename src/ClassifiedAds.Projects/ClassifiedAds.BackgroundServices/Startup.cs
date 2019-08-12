232d56dfdd7b870526a350ab2305ea3c23f1ac6f
Phong Nguyen <nguyendoanphong.it@gmail.com>
2019-08-12 12:39:27 +0700

    Background Services with Hangfire

---
+++ src/ClassifiedAds.Projects/ClassifiedAds.BackgroundServices/Startup.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using ClassifiedAds.BackgroundServices.Jobs;
+using Hangfire;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+
+namespace ClassifiedAds.BackgroundServices
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration, IHostingEnvironment env)
+        {
+            Configuration = configuration;
+
+            Directory.CreateDirectory(Path.Combine(env.ContentRootPath, "logs"));
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.File(Path.Combine(env.ContentRootPath, "logs", "log.txt"),
+                    fileSizeLimitBytes: 10 * 1024 * 1024,
+                    rollOnFileSizeLimit: true,
+                    shared: true,
+                    flushToDiskInterval: TimeSpan.FromSeconds(1))
+                .CreateLogger();
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddHangfire(x =>
+            {
+                var options = new Hangfire.SqlServer.SqlServerStorageOptions
+                {
+                    PrepareSchemaIfNecessary = true
+                };
+                x.UseSqlServerStorage(Configuration.GetConnectionString("ClassifiedAds"), options);
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHangfireDashboard();
+            app.UseHangfireServer();
+
+            RecurringJob.AddOrUpdate<SendEmail>(job => job.Run(), Cron.Minutely);
+            RecurringJob.AddOrUpdate<SendSms>(job => job.Run(), Cron.Minutely);
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+            });
+        }
+    }
+}
