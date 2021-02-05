c09dbeb6af2b37b956a08d0edcef3d431507d7e5
Phong Nguyen <nguyendoanphong.it@gmail.com>
2021-02-05 11:11:37 +0700

    (#96) Architectural Tests

---
+++ src/Monolith/ClassifiedAds.ArchTests/ApplicationTests.cs
@@ -0,0 +1,31 @@
+using ClassifiedAds.Application;
+using NetArchTest.Rules;
+using Xunit;
+
+namespace ClassifiedAds.ArchTests
+{
+    public class ApplicationTests
+    {
+        [Fact]
+        public void ApplicationShouldNotHaveDependencyOnInfrastructure()
+        {
+            var result = Types.InAssembly(typeof(Dispatcher).Assembly)
+                .ShouldNot()
+                .HaveDependencyOn("ClassifiedAds.Infrastructure")
+                .GetResult();
+
+            Assert.True(result.IsSuccessful);
+        }
+
+        [Fact]
+        public void ApplicationShouldNotHaveDependencyOnPersistence()
+        {
+            var result = Types.InAssembly(typeof(Dispatcher).Assembly)
+                .ShouldNot()
+                .HaveDependencyOn("ClassifiedAds.Persistence")
+                .GetResult();
+
+            Assert.True(result.IsSuccessful);
+        }
+    }
+}
