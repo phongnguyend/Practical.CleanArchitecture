34d48a61340e91ed4f832bb391a505075003d2eb
Phong Nguyen <nguyendoanphong.it@gmail.com>
2021-02-05 10:29:51 +0700

    arch tests

---
+++ src/Monolith/ClassifiedAds.ArchTests/DomainTests.cs
@@ -0,0 +1,42 @@
+using ClassifiedAds.Domain.Entities;
+using NetArchTest.Rules;
+using Xunit;
+
+namespace ClassifiedAds.ArchTests
+{
+    public class DomainTests
+    {
+        [Fact]
+        public void DomainShouldNotHaveDependencyOnApplication()
+        {
+            var result = Types.InAssembly(typeof(Entity<>).Assembly)
+                .ShouldNot()
+                .HaveDependencyOn("ClassifiedAds.Application")
+                .GetResult();
+
+            Assert.True(result.IsSuccessful);
+        }
+
+        [Fact]
+        public void DomainShouldNotHaveDependencyOnInfrastructure()
+        {
+            var result = Types.InAssembly(typeof(Entity<>).Assembly)
+                .ShouldNot()
+                .HaveDependencyOn("ClassifiedAds.Infrastructure")
+                .GetResult();
+
+            Assert.True(result.IsSuccessful);
+        }
+
+        [Fact]
+        public void DomainShouldNotHaveDependencyOnPersistence()
+        {
+            var result = Types.InAssembly(typeof(Entity<>).Assembly)
+                .ShouldNot()
+                .HaveDependencyOn("ClassifiedAds.Persistence")
+                .GetResult();
+
+            Assert.True(result.IsSuccessful);
+        }
+    }
+}
