19dfa3b9e2427bce7494805f7b06a1ac466cf285
Phong Nguyen <nguyendoanphong.it@gmail.com>
2022-05-05 23:03:04 +0700

    refactoring: remove unused code

--- src/Microservices/Common/ClassifiedAds.Application/Common/Dispatcher.cs
+++ src/Microservices/Common/ClassifiedAds.Application/Common/Dispatcher.cs
@@ -13,11 +13,6 @@ namespace ClassifiedAds.Application
             _provider = provider;
         }
 
-        public void Dispatch(ICommand command)
-        {
-            DispatchAsync(command).GetAwaiter().GetResult();
-        }
-
         public async Task DispatchAsync(ICommand command, CancellationToken cancellationToken = default)
         {
             Type type = typeof(ICommandHandler<>);
@@ -26,11 +21,6 @@ namespace ClassifiedAds.Application
 
             dynamic handler = _provider.GetService(handlerType);
             await handler.HandleAsync((dynamic)command, cancellationToken);
-        }
-
-        public T Dispatch<T>(IQuery<T> query)
-        {
-            return DispatchAsync(query).GetAwaiter().GetResult();
         }
 
         public async Task<T> DispatchAsync<T>(IQuery<T> query, CancellationToken cancellationToken = default)
