9db04e74fdb977aff826b9420297f10d59e11a3d
Phong Nguyen <nguyendoanphong.it@gmail.com>
2022-05-06 23:50:40 +0700

    (#118) Saga Distributed Transactions

---
+++ src/Monolith/ClassifiedAds.Domain/Entities/DistributedTransactionProperty.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ClassifiedAds.Domain.Entities
+{
+    public class DistributedTransactionProperty : Entity<Guid>
+    {
+        public string Name { get; set; }
+
+        public string Value { get; set; }
+
+        public Guid DistributedTransactionId { get; set; }
+    }
+}
---
+++ src/Monolith/ClassifiedAds.Persistence/MappingConfigurations/DistributedTransactionDetailConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using ClassifiedAds.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ClassifiedAds.Persistence.MappingConfigurations
+{
+    public class DistributedTransactionDetailConfiguration : IEntityTypeConfiguration<DistributedTransactionDetail>
+    {
+        public void Configure(EntityTypeBuilder<DistributedTransactionDetail> builder)
+        {
+            builder.ToTable("DistributedTransactionDetails");
+            builder.Property(x => x.Id).HasDefaultValueSql("newsequentialid()");
+        }
+    }
+}
