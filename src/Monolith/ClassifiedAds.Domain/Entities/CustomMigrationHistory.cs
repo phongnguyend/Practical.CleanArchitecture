adcc0296316ff165cbe6e241c6681d28f1dd44a3
Phong Nguyen <nguyendoanphong.it@gmail.com>
2022-11-06 22:13:50 +0700

    Custom Migration History

---
+++ src/Monolith/ClassifiedAds.Domain/Entities/CustomMigrationHistory.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace ClassifiedAds.Domain.Entities
+{
+    public class CustomMigrationHistory : Entity<Guid>
+    {
+        public string MigrationName { get; set; }
+    }
+}
