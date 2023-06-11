ede1e1991b6119257207ed3c6414fd28134d191a
Phong Nguyen <nguyendoanphong.it@gmail.com>
2023-06-11 11:59:34 +0700

    remove Startup.cs

--- src/Microservices/Services.Product/ClassifiedAds.Services.Product.Api/Program.cs
+++ src/Microservices/Services.Product/ClassifiedAds.Services.Product.Api/Program.cs
@@ -1,7 +1,19 @@
-using ClassifiedAds.Infrastructure.Logging;
-using Microsoft.AspNetCore;
+﻿using ClassifiedAds.Infrastructure.Logging;
+using ClassifiedAds.Infrastructure.Monitoring;
+using ClassifiedAds.Infrastructure.Web.Filters;
+using ClassifiedAds.Services.Product.ConfigurationOptions;
+using ClassifiedAds.Services.Product.RateLimiterPolicies;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
+using Polly;
+using System;
+using System.Reflection;
 
 namespace ClassifiedAds.Services.Product;
 
@@ -9,14 +21,116 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        CreateWebHostBuilder(args).Build().Run();
-    }
+        var builder = WebApplication.CreateBuilder(args);
 
-    public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-        WebHost.CreateDefaultBuilder(args)
-            .UseStartup<Startup>()
-            .UseClassifiedAdsLogger(configuration =>
+        // Add services to the container.
+        var services = builder.Services;
+        var configuration = builder.Configuration;
+
+        builder.WebHost.UseClassifiedAdsLogger(configuration =>
+        {
+            return new LoggingOptions();
+        });
+
+        var appSettings = new AppSettings();
+        configuration.Bind(appSettings);
+
+        appSettings.ConnectionStrings.MigrationsAssembly = Assembly.GetExecutingAssembly().GetName().Name;
+
+        services.AddMonitoringServices(appSettings.Monitoring);
+
+        services.AddControllers(configure =>
+        {
+            configure.Filters.Add(typeof(GlobalExceptionFilter));
+        })
+        .ConfigureApiBehaviorOptions(options =>
+        {
+        })
+        .AddJsonOptions(options =>
+        {
+        });
+
+        services.AddCors(options =>
+        {
+            options.AddPolicy("AllowAnyOrigin", builder => builder
+                .AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader());
+        });
+
+        services.AddDateTimeProvider();
+        services.AddApplicationServices();
+
+        services.AddHtmlGenerator();
+        services.AddDinkToPdfConverter();
+
+        services.AddProductModule(appSettings);
+        services.AddHostedServicesProductModule();
+
+        services.AddAuthentication(options =>
+        {
+            options.DefaultScheme = appSettings.IdentityServerAuthentication.Provider switch
             {
-                return new LoggingOptions();
-            });
+                "OpenIddict" => "OpenIddict",
+                _ => JwtBearerDefaults.AuthenticationScheme
+            };
+        })
+        .AddJwtBearer(options =>
+        {
+            options.Authority = appSettings.IdentityServerAuthentication.Authority;
+            options.Audience = appSettings.IdentityServerAuthentication.ApiName;
+            options.RequireHttpsMetadata = appSettings.IdentityServerAuthentication.RequireHttpsMetadata;
+        })
+        .AddJwtBearer("OpenIddict", options =>
+        {
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateAudience = false,
+                ValidIssuer = appSettings.IdentityServerAuthentication.OpenIddict.IssuerUri,
+                TokenDecryptionKey = new X509SecurityKey(appSettings.IdentityServerAuthentication.OpenIddict.TokenDecryptionCertificate.FindCertificate()),
+                IssuerSigningKey = new X509SecurityKey(appSettings.IdentityServerAuthentication.OpenIddict.IssuerSigningCertificate.FindCertificate()),
+            };
+        });
+
+        services.AddRateLimiter(options =>
+        {
+            options.AddPolicy<string, DefaultRateLimiterPolicy>(RateLimiterPolicyNames.DefaultPolicy);
+        });
+
+        services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
+        services.AddDaprClient();
+
+        // Configure the HTTP request pipeline.
+        var app = builder.Build();
+
+        Policy.Handle<Exception>().WaitAndRetry(new[]
+        {
+            TimeSpan.FromSeconds(10),
+            TimeSpan.FromSeconds(20),
+            TimeSpan.FromSeconds(30),
+        })
+        .Execute(() =>
+        {
+            app.MigrateProductDb();
+        });
+
+        if (builder.Environment.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+
+        app.UseRouting();
+
+        app.UseCors("AllowAnyOrigin");
+
+        app.UseAuthentication();
+        app.UseAuthorization();
+
+        app.UseRateLimiter();
+
+        app.MapControllers();
+
+        app.Run();
+    }
 }
