75dc60bf1822e86f77630b501984a9a551dcaedd
Phong Nguyen <nguyendoanphong.it@gmail.com>
2023-08-21 11:06:02 +0700

    refactor configuration providers

--- src/IdentityServer/Duende/ClassifiedAds.Infrastructure/Configuration/HashiCorpVaultConfigurationProvider.cs
+++ src/IdentityServer/Duende/ClassifiedAds.Infrastructure/Configuration/HashiCorpVaultConfigurationProvider.cs
@@ -5,35 +5,91 @@ using VaultSharp.V1.AuthMethods;
 using VaultSharp.V1.AuthMethods.Token;
 using VaultSharp.V1.AuthMethods.UserPass;
 
-namespace ClassifiedAds.Infrastructure.Configuration
+namespace ClassifiedAds.Infrastructure.Configuration;
+
+internal class HashiCorpVaultConfigurationProvider : ConfigurationProvider
 {
-    internal class HashiCorpVaultConfigurationProvider : ConfigurationProvider
+    private readonly HashiCorpVaultOptions _options;
+
+    public HashiCorpVaultConfigurationProvider(HashiCorpVaultOptions options)
     {
-        private readonly HashiCorpVaultOptions _options;
+        _options = options;
+    }
 
-        public HashiCorpVaultConfigurationProvider(HashiCorpVaultOptions options)
+    public override void Load()
+    {
+        IAuthMethodInfo authMethod = null;
+
+        if (_options.AuthMethod == "Token")
         {
-            _options = options;
+            authMethod = new TokenAuthMethodInfo(_options.Auth.Token.Token);
+        }
+        else if (_options.AuthMethod == "UserPass")
+        {
+            authMethod = new UserPassAuthMethodInfo(_options.Auth.UserPass.UserName, _options.Auth.UserPass.Password);
         }
 
-        public override void Load()
-        {
-            IAuthMethodInfo authMethod = null;
+        var vaultClientSettings = new VaultClientSettings(_options.Address, authMethod);
+        var client = new VaultClient(vaultClientSettings);
+        var secrets = client.V1.Secrets.KeyValue.V2.ReadSecretAsync(path: _options.SecretPath, mountPoint: _options.SecretEnginePath)
+            .GetAwaiter().GetResult();
+        Data = secrets.Data.Data.ToDictionary(x => x.Key, x => x.Value.ToString());
+    }
+}
 
-            if (_options.AuthMethod == "Token")
-            {
-                authMethod = new TokenAuthMethodInfo(_options.Auth.Token.Token);
-            }
-            else if (_options.AuthMethod == "UserPass")
-            {
-                authMethod = new UserPassAuthMethodInfo(_options.Auth.UserPass.UserName, _options.Auth.UserPass.Password);
-            }
+public class HashiCorpVaultOptions
+{
+    public bool IsEnabled { get; set; }
 
-            var vaultClientSettings = new VaultClientSettings(_options.Address, authMethod);
-            var client = new VaultClient(vaultClientSettings);
-            var secrets = client.V1.Secrets.KeyValue.V2.ReadSecretAsync(path: _options.SecretPath, mountPoint: _options.SecretEnginePath)
-                .GetAwaiter().GetResult();
-            Data = secrets.Data.Data.ToDictionary(x => x.Key, x => x.Value.ToString());
-        }
+    public string Address { get; set; }
+
+    public string SecretEnginePath { get; set; }
+
+    public string SecretPath { get; set; }
+
+    public string AuthMethod { get; set; }
+
+    public HashiCorpVaultAuthOptions Auth { get; set; }
+}
+
+public class HashiCorpVaultAuthOptions
+{
+    public HashiCorpVaultTokenAuthOptions Token { get; set; }
+
+    public HashiCorpVaultUserPassAuthOptions UserPass { get; set; }
+}
+
+public class HashiCorpVaultUserPassAuthOptions
+{
+    public string UserName { get; set; }
+
+    public string Password { get; set; }
+}
+
+public class HashiCorpVaultTokenAuthOptions
+{
+    public string Token { get; set; }
+}
+
+public class HashiCorpVaultConfigurationSource : IConfigurationSource
+{
+    private readonly HashiCorpVaultOptions _options;
+
+    public HashiCorpVaultConfigurationSource(HashiCorpVaultOptions options)
+    {
+        _options = options;
+    }
+
+    public IConfigurationProvider Build(IConfigurationBuilder builder)
+    {
+        return new HashiCorpVaultConfigurationProvider(_options);
+    }
+}
+
+public static class HashiCorpVaultConfigurationExtensions
+{
+    public static IConfigurationBuilder AddHashiCorpVault(this IConfigurationBuilder builder, HashiCorpVaultOptions options)
+    {
+        return builder.Add(new HashiCorpVaultConfigurationSource(options));
     }
 }
--- src/IdentityServer/IdentityServer4/ClassifiedAds.Infrastructure/Configuration/SqlConfigurationProvider.cs
+++ src/IdentityServer/IdentityServer4/ClassifiedAds.Infrastructure/Configuration/SqlConfigurationProvider.cs
@@ -1,52 +1,86 @@
 ﻿using CryptographyHelper;
 using CryptographyHelper.AsymmetricAlgorithms;
+using CryptographyHelper.Certificates;
 using Dapper;
 using Microsoft.Data.SqlClient;
 using Microsoft.Extensions.Configuration;
 using System.Linq;
 
-namespace ClassifiedAds.Infrastructure.Configuration
+namespace ClassifiedAds.Infrastructure.Configuration;
+
+public class SqlConfigurationProvider : ConfigurationProvider
 {
-    public class SqlConfigurationProvider : ConfigurationProvider
+    private readonly SqlServerOptions _options;
+
+    public SqlConfigurationProvider(SqlServerOptions options)
     {
-        private readonly SqlServerOptions _options;
-
-        public SqlConfigurationProvider(SqlServerOptions options)
-        {
-            _options = options;
-        }
-
-        public override void Load()
-        {
-            using (var conn = new SqlConnection(_options.ConnectionString))
-            {
-                conn.Open();
-                var data = conn.Query<ConfigurationEntry>(_options.SqlQuery).ToList();
-
-                var cert = data.Any(x => x.IsSensitive)
-                    ? _options.Certificate.FindCertificate()
-                    : null;
-
-                foreach (var entry in data)
-                {
-                    if (entry.IsSensitive)
-                    {
-                        var decrypted = entry.Value.FromBase64String().UseRSA(cert).Decrypt();
-                        entry.Value = decrypted.GetString();
-                    }
-                }
-
-                Data = data.ToDictionary(c => c.Key, c => c.Value);
-            }
-        }
+        _options = options;
     }
 
-    public class ConfigurationEntry
+    public override void Load()
     {
-        public string Key { get; set; }
+        using (var conn = new SqlConnection(_options.ConnectionString))
+        {
+            conn.Open();
+            var data = conn.Query<ConfigurationEntry>(_options.SqlQuery).ToList();
 
-        public string Value { get; set; }
+            var cert = data.Any(x => x.IsSensitive)
+                ? _options.Certificate.FindCertificate()
+                : null;
 
-        public bool IsSensitive { get; set; }
+            foreach (var entry in data)
+            {
+                if (entry.IsSensitive)
+                {
+                    var decrypted = entry.Value.FromBase64String().UseRSA(cert).Decrypt();
+                    entry.Value = decrypted.GetString();
+                }
+            }
+
+            Data = data.ToDictionary(c => c.Key, c => c.Value);
+        }
+    }
+}
+
+public class ConfigurationEntry
+{
+    public string Key { get; set; }
+
+    public string Value { get; set; }
+
+    public bool IsSensitive { get; set; }
+}
+
+public class SqlServerOptions
+{
+    public bool IsEnabled { get; set; }
+
+    public string ConnectionString { get; set; }
+
+    public string SqlQuery { get; set; }
+
+    public CertificateOption Certificate { get; set; }
+}
+
+public class SqlConfigurationSource : IConfigurationSource
+{
+    private readonly SqlServerOptions _options;
+
+    public SqlConfigurationSource(SqlServerOptions options)
+    {
+        _options = options;
+    }
+
+    public IConfigurationProvider Build(IConfigurationBuilder builder)
+    {
+        return new SqlConfigurationProvider(_options);
+    }
+}
+
+public static class SqlConfigurationExtensions
+{
+    public static IConfigurationBuilder AddSqlServer(this IConfigurationBuilder builder, SqlServerOptions options)
+    {
+        return builder.Add(new SqlConfigurationSource(options));
     }
 }
