0859344d88f4f635c6206214e09c66da3cc8336b
Phong Nguyen <nguyendoanphong.it@gmail.com>
2023-10-24 12:35:02 +0700

    Data Archiving: Create Indexes

--- src/ModularMonolith/ClassifiedAds.Modules.Notification/MappingConfigurations/SmsMessageConfiguration.cs
+++ src/ModularMonolith/ClassifiedAds.Modules.Notification/MappingConfigurations/SmsMessageConfiguration.cs
@@ -10,6 +10,8 @@ public class SmsMessageConfiguration : IEntityTypeConfiguration<SmsMessage>
     {
         builder.ToTable("SmsMessages");
         builder.Property(x => x.Id).HasDefaultValueSql("newsequentialid()");
+        builder.HasIndex(x => x.SentDateTime).IncludeProperties(x => new { x.ExpiredDateTime, x.AttemptCount, x.MaxAttemptCount, x.NextAttemptDateTime });
+        builder.HasIndex(x => x.CreatedDateTime);
     }
 }
 
@@ -18,5 +20,6 @@ public class ArchivedSmsMessageConfiguration : IEntityTypeConfiguration<Archived
     public void Configure(EntityTypeBuilder<ArchivedSmsMessage> builder)
     {
         builder.ToTable("ArchivedSmsMessages");
+        builder.HasIndex(x => x.CreatedDateTime);
     }
 }
