eeae3cc0c3dee1ec9337a6160bc813d2e88f554e
Phong Nguyen <nguyendoanphong.it@gmail.com>
2024-05-16 13:52:08 +0700

    .Net 8 - [FromForm] with Minimal API

---
+++ src/Microservices/Services.Product/ClassifiedAds.Services.Product.Api/Endpoints/ImportCsvRequest.cs
@@ -0,0 +1,45 @@
+﻿using ClassifiedAds.CrossCuttingConcerns.Csv;
+using ClassifiedAds.Infrastructure.Web.MinimalApis;
+using ClassifiedAds.Services.Product.Models;
+using ClassifiedAds.Services.Product.RateLimiterPolicies;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.OpenApi.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ClassifiedAds.Services.Product.Api.Endpoints;
+
+public class ImportCsvRequest
+{
+    public IFormFile FormFile { get; set; }
+}
+
+public class ImportCsvRequestHandler : IEndpointHandler
+{
+    public static void MapEndpoint(IEndpointRouteBuilder builder)
+    {
+        builder.MapPost("api/products/importcsv", HandleAsync)
+        .RequireAuthorization()
+        .RequireRateLimiting(RateLimiterPolicyNames.DefaultPolicy)
+        .WithName("ImportCsv")
+        .Produces<CreateProductResponse>(StatusCodes.Status200OK, contentType: "application/json")
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithOpenApi(operation => new OpenApiOperation(operation)
+        {
+            Tags = new List<OpenApiTag> { new OpenApiTag { Name = "Products" } }
+        })
+        .DisableAntiforgery();
+    }
+
+    private static Task<IResult> HandleAsync(ICsvReader<ProductModel> productCsvReader, [FromForm] ImportCsvRequest request)
+    {
+        using var stream = request.FormFile.OpenReadStream();
+        var products = productCsvReader.Read(stream);
+
+        // TODO: import to database
+        return Task.FromResult(Results.Ok(products));
+    }
+}
--- src/Microservices/Services.Product/ClassifiedAds.Services.Product.Api/Models/UploadFileModel.cs
+++
@@ -1,8 +0,0 @@
-﻿using Microsoft.AspNetCore.Http;
-
-namespace ClassifiedAds.Services.Product.Models;
-
-public class UploadFileModel
-{
-    public IFormFile FormFile { get; set; }
-}
