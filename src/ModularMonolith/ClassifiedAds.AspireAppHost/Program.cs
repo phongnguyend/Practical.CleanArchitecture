9f68df82bc21715e15db9d554bd8ab85fa4934bc
Phong Nguyen <nguyendoanphong.it@gmail.com>
2024-06-20 12:15:27 +0700

    (#239) Add Aspire

---
+++ src/ModularMonolith/ClassifiedAds.AspireAppHost/Program.cs
@@ -0,0 +1,10 @@
+﻿var builder = DistributedApplication.CreateBuilder(args);
+
+var migrator = builder.AddProject<Projects.ClassifiedAds_Migrator>("ClassifiedAds-Migrator");
+var background = builder.AddProject<Projects.ClassifiedAds_BackgroundServer>("ClassifiedAds-BackgroundServer");
+var webApi = builder.AddProject<Projects.ClassifiedAds_WebAPI>("ClassifiedAds-WebAPI");
+
+var identityServer = builder
+    .AddExecutable("ClassifiedAds-IdentityServer", "dotnet", "../../IdentityServer/IdentityServer4/ClassifiedAds.IdentityServer", "run", $"--urls=https://localhost:44367");
+
+builder.Build().Run();
