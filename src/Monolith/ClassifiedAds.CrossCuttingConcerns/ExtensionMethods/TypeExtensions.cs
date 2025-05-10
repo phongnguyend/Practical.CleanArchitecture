f94b1df945cd310bacfc4c62d6ac3a9843a50f45
Phong Nguyen <nguyendoanphong.it@gmail.com>
2025-05-10 17:04:38 +0700

    Remove Unused Code

--- src/Monolith/ClassifiedAds.CrossCuttingConcerns/ExtensionMethods/TypeExtensions.cs
+++ src/Monolith/ClassifiedAds.CrossCuttingConcerns/ExtensionMethods/TypeExtensions.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Linq;
-using System.Text;
 
 namespace ClassifiedAds.CrossCuttingConcerns.ExtensionMethods;
 
@@ -28,31 +27,5 @@ public static class TypeExtensions
         return type.IsGenericType && !type.GenericTypeArguments.Any()
             ? type.MakeGenericType(typeArguments)
             : type;
-    }
-
-    public static string GenerateMappingCode(this Type type)
-    {
-        var names = type.GetProperties().Select(x => x.Name);
-
-        var text1 = new StringBuilder();
-        var text2 = new StringBuilder();
-        var text3 = new StringBuilder();
-        var text4 = new StringBuilder();
-
-        foreach (var name in names)
-        {
-            text1.Append($"a.{name} = {name};{Environment.NewLine}");
-            text2.Append($"{name} = b.{name};{Environment.NewLine}");
-            text3.Append($"{name} = b.{name},{Environment.NewLine}");
-            text4.Append($"a.{name} = b.{name};{Environment.NewLine}");
-        }
-
-        return text1.ToString()
-            + "--------------------------------------" + Environment.NewLine
-            + text2.ToString()
-            + "--------------------------------------" + Environment.NewLine
-            + text3.ToString()
-            + "--------------------------------------" + Environment.NewLine
-            + text4.ToString();
     }
 }
