86c443bb13a3ae335d0dd058c847199f330428e5
Phong Nguyen <nguyendoanphong.it@gmail.com>
2025-11-13 14:01:15 +0700

    (#301) Upgrade .Net 10

---
+++ src/IdentityServers/OpenIddict/ClassifiedAds.Migrator/Migrations/OpenIddictDb/20251113064145_UpgradeV7.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ClassifiedAds.Migrator.Migrations.OpenIddictDb
+{
+    /// <inheritdoc />
+    public partial class UpgradeV7 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Type",
+                table: "OpenIddictTokens",
+                type: "nvarchar(150)",
+                maxLength: 150,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50,
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Type",
+                table: "OpenIddictTokens",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(150)",
+                oldMaxLength: 150,
+                oldNullable: true);
+        }
+    }
+}
